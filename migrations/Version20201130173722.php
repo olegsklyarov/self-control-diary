f6ac185aaccfa8a809676c591eb44df0a1f1b857
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2020-11-30 20:45:14 +0300

    #4 mencho mantra entity, repository, migratin and fixture

---
+++ migrations/Version20201130173722.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201130173722 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $tableMenchoMantra = $schema->createTable('mencho_mantra');
+        $tableMenchoMantra->addColumn('uuid', 'uuid');
+        $tableMenchoMantra->setPrimaryKey(['uuid']);
+
+        $tableMenchoMantra->addColumn('name', 'string');
+        $tableMenchoMantra->addUniqueIndex(['name']);
+
+        $tableMenchoMantra->addColumn('level', 'integer');
+        $tableMenchoMantra->addColumn('description', 'text', ['notnull' => false]);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $schema->dropTable('mencho_mantra');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\MenchoMantra;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -18,6 +19,17 @@ class AppFixtures extends Fixture
             password_hash(self::TEST_USER_PASSWORD, PASSWORD_DEFAULT),
         );
         $manager->persist($user);
+
+        $manager->persist(new MenchoMantra('Будда Шакьямуни', 1));
+        $manager->persist(new MenchoMantra('Будда Медицины', 1));
+        $manager->persist(new MenchoMantra('Амитаба', 1));
+        $manager->persist(new MenchoMantra('Ченрезиг', 1));
+        $manager->persist(new MenchoMantra('Ваджрапани', 1));
+        $manager->persist(new MenchoMantra('Хайягрива', 1));
+        $manager->persist(new MenchoMantra('Падмасамбава/Гуру Ринпоче', 2));
+        $manager->persist(new MenchoMantra('Красная Дакиня Симкхамукха', 2));
+        $manager->persist(new MenchoMantra('Зеленая Тара', 2));
+        $manager->persist(new MenchoMantra('Белый Махакала', 2));
         $manager->flush();
     }
 }
