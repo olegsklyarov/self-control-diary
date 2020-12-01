98662c685478e9ce689fcfa3306544a37caf7fe3
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2020-12-01 19:50:27 +0300

    #13 generate api token on login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,14 +16,21 @@ class SecurityController extends AbstractController
     {
         /** @var User $user */
         $user = $this->getUser();
+
+        $apiToken = bin2hex(random_bytes(60));
+        $apiTokenExpiresAt = new \DateTimeImmutable('+1 day');
+
         $user->setLastVisitAt(new \DateTimeImmutable());
+        $user->setApiToken($apiToken);
+        $user->setApiTokenExpiresAt($apiTokenExpiresAt);
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($user);
         $em->flush();
 
         return $this->json([
-            'user' => 'Welcome, ' . $user->getUsername(),
+            'token' => $apiToken,
+            'expires_at' => $apiTokenExpiresAt->format(\DateTimeImmutable::ISO8601),
         ]);
     }
 }
