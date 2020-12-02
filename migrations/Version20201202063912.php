a69c15aa08f56257c392c05f7adb6a4a4e9b9c6e
ZhdanovO <oleg.zhdanov88@gmail.com>
2020-12-02 18:19:01 +0300

    [#12] new migration for running table

---
+++ migrations/Version20201202063912.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201202063912 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Create running table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $tableRunning = $schema->createTable('running');
+
+        $tableRunning->addColumn('diary_uuid', 'uuid');
+        $tableRunning->setPrimaryKey(['diary_uuid']);
+
+        $tableRunning->addColumn('distance_meters', 'integer');
+        $tableRunning->addColumn('time_minutes', 'integer');
+        $tableRunning->addColumn('temperature_celsius', 'integer');
+        $tableRunning->addColumn('health_notes', 'text', ['notnull' => false]);
+        $tableRunning->addColumn('party', 'text', ['notnull' => false]);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $schema->dropTable('running');
+    }
+}
