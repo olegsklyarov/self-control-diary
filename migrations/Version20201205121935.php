6a95cb15b6e1771a6deed8d49232568685d7b43e
ZhdanovO <oleg.zhdanov88@gmail.com>
2020-12-07 07:46:31 +0300

    [#11] Add requested changes https://github.com/olegsklyarov/self-control-diary/pull/15#partial-pull-merging

--- migrations/Version20201205121935.php
+++ migrations/Version20201205121935.php
@@ -7,36 +7,33 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20201205121935 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return 'Create mencho_samaya table';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
-        $tableRunning = $schema->createTable('mencho_samaya');
+        $tableMenchoSamaya = $schema->createTable('mencho_samaya');
 
-        $tableRunning->addColumn('uuid', 'uuid');
-        $tableRunning->setPrimaryKey(['uuid']);
+        $tableMenchoSamaya->addColumn('uuid', 'uuid');
+        $tableMenchoSamaya->setPrimaryKey(['uuid']);
 
-        $tableRunning->addColumn('diary_uuid', 'uuid');
-        $tableRunning->addColumn('mantra_uuid', 'uuid');
-        $tableRunning->addColumn('count', 'integer');
-        $tableRunning->addColumn('time_minutes', 'integer');
+        $tableMenchoSamaya->addColumn('diary_uuid', 'uuid');
+        $tableMenchoSamaya->addColumn('mantra_uuid', 'uuid');
+        $tableMenchoSamaya->addColumn('count', 'integer');
+        $tableMenchoSamaya->addColumn('time_minutes', 'integer', ['notnull' => false]);
 
         $tableDiary = $schema->getTable('diary');
-        $tableRunning->addForeignKeyConstraint($tableDiary, ['diary_uuid'], ['uuid']);
+        $tableMenchoSamaya->addForeignKeyConstraint($tableDiary, ['diary_uuid'], ['uuid']);
 
         $tableMenchoMantra = $schema->getTable('mencho_mantra');
-        $tableRunning->addForeignKeyConstraint($tableMenchoMantra, ['mantra_uuid'], ['uuid']);
+        $tableMenchoSamaya->addForeignKeyConstraint($tableMenchoMantra, ['mantra_uuid'], ['uuid']);
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $schema->dropTable('mencho_samaya');
     }
