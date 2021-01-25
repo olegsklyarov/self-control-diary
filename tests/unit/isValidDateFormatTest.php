16fd56ecc4505fb7f12972cf95b9b47e108e1d66
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-01-25 14:30:08 +0300

    [SCD-44] remove phpunit-bridge, add .env.test to gitignore

--- tests/unit/isValidDateFormatTest.php
+++ tests/unit/isValidDateFormatTest.php
@@ -11,5 +11,7 @@ class isValidDateFormatTest extends \Codeception\Test\Unit
     public function testIsValidDateFormat()
     {
         self::assertTrue(Util::isValidDateFormat('2021-01-26'));
+        self::assertFalse(Util::isValidDateFormat(''));
+        self::assertFalse(Util::isValidDateFormat('57079b9d-47da-4071-802f-d32b35068ae5'));
     }
 }
