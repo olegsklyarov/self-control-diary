75d1f1b944c229a027256e3e90a87ff979878ad1
ZhdanovO <oleg.zhdanov88@gmail.com>
2021-02-04 08:14:14 +0300

    [SCD-53] Add empty DiaryGetCest.

---
+++ tests/Api/DiaryGetCest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Api;
+use App\Tests\ApiTester;
+
+class DiaryGetCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+}
