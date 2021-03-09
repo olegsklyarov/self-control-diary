9de0e8aa6355e5a5a8c5f785b39cc373c8aa1297
ZhdanovO <oleg.zhdanov88@gmail.com>
2021-03-09 07:08:56 +0300

    [SCD-64] Edit MenchoSamayaService (remove try/catch)

--- src/Service/Mencho/MenchoSamayaService.php
+++ src/Service/Mencho/MenchoSamayaService.php
@@ -7,8 +7,6 @@ namespace App\Service\Mencho;
 use App\Entity\Diary;
 use App\Entity\MenchoMantra;
 use App\Entity\MenchoSamaya;
-use App\Service\Mencho\Exception\DiaryNotFoundException;
-use App\Service\Mencho\Exception\MantraNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
 
 final class MenchoSamayaService
@@ -17,23 +15,15 @@ final class MenchoSamayaService
     {
     }
 
-    public function findByDiaryAndMantra(?Diary $diary, ?MenchoMantra $menchoMantra): ?MenchoSamaya
+    public function findByDiaryAndMantra(Diary $diary, MenchoMantra $menchoMantra): ?MenchoSamaya
     {
         $menchoSamaya = null;
-        try {
-            if (null === $diary) {
-                throw new DiaryNotFoundException();
-            }
-            if (null === $menchoMantra) {
-                throw new MantraNotFoundException();
-            }
-            /** @var MenchoSamaya|null $menchoSamaya */
-            $menchoSamaya = $this->entityManager->getRepository(MenchoSamaya::class)->findOneBy([
-                'diary' => $diary,
-                'menchoMantra' => $menchoMantra,
-            ]);
-        } catch (\Throwable $e) {
-        }
+
+        /** @var MenchoSamaya|null $menchoSamaya */
+        $menchoSamaya = $this->entityManager->getRepository(MenchoSamaya::class)->findOneBy([
+            'diary' => $diary,
+            'menchoMantra' => $menchoMantra,
+        ]);
 
         return $menchoSamaya;
     }
