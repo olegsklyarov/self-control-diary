5e49b51b658191332f708265a658212298c6818a
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-03-29 10:00:33 +0300

    [SCD-109] console command signu:send-verification-email stub
    Signed-off-by: Oleg Sklyarov <oleg.sklyarov@gmail.com>

---
+++ src/Command/SendVerificationEmailCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class SendVerificationEmailCommand extends Command
+{
+    protected static $defaultName = 'signup:send-verification-email';
+
+    public function configure(): void
+    {
+        $this->setDescription('Send email using SendPulse service to verify user email');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        echo 'Hello from console command!' . PHP_EOL;
+
+        return 0;
+    }
+}
