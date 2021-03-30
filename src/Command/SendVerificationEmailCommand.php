57c5b1ee8a26b3475fec2bb89c367c610c280af2
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-03-30 21:03:21 +0300

    [SCD-109] console command options
    Signed-off-by: Oleg Sklyarov <oleg.sklyarov@gmail.com>

--- src/Command/SendVerificationEmailCommand.php
+++ src/Command/SendVerificationEmailCommand.php
@@ -7,11 +7,17 @@ namespace App\Command;
 use App\Service\EmailNotificationInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class SendVerificationEmailCommand extends Command
 {
     protected static $defaultName = 'signup:send-verification-email';
+
+    private const DEFAULT_VERIFICATION_LINK = 'https://ya.ru';
+
+    private const OPTION_EMAIL = 'email';
+    private const OPTION_PHONE = 'phone';
 
     public function __construct(private EmailNotificationInterface $emailNotification)
     {
@@ -20,16 +26,36 @@ final class SendVerificationEmailCommand extends Command
 
     public function configure(): void
     {
-        $this->setDescription('Send email using SendPulse service to verify user email');
+        $this
+            ->setDescription('Send email using SendPulse service to verify user email')
+            ->addOption(
+                self::OPTION_EMAIL,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'email address to send verification link',
+                null
+            )
+            ->addOption(
+                self::OPTION_PHONE,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'phone number of the user',
+            );
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        echo 'Sending verification email...' . PHP_EOL;
+        $email = $input->getOption(self::OPTION_EMAIL);
+        if (null === $email) {
+            $output->writeln('please specify email parameter');
 
-        $this->emailNotification->sendVerificationEmail('oleg.sklyarov@gmail.com', null, 'https://ya.ru');
+            return 0;
+        }
 
-        echo 'DONE' . PHP_EOL;
+        $phone = $input->getOption(self::OPTION_PHONE);
+        $output->writeln(sprintf('Sending verification link to email %s...', $email));
+        $this->emailNotification->sendVerificationEmail($email, $phone, self::DEFAULT_VERIFICATION_LINK);
+        $output->writeln('DONE');
 
         return 0;
     }
