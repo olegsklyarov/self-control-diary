e5a3b78f42ddb23be0607ad76602b154a2ef5348
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-04-01 14:57:54 +0300

    [SCD-60] composer require nelmio/api-doc-bundle:3.*
    Signed-off-by: Oleg Sklyarov <oleg.sklyarov@gmail.com>

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
