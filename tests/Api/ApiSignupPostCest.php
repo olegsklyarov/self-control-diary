f78b2144fbbcae12d42d10e230e5b54a6db06ec3
ZhdanovO <oleg.zhdanov88@gmail.com>
2021-04-05 08:01:54 +0300

    [SCD-106] Add testInvalidEmail, testEmailAlreadySignedUp, testVerificationEmailExist

--- tests/Api/ApiSignupPostCest.php
+++ tests/Api/ApiSignupPostCest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Api;
 
+use App\Entity\Lead;
 use App\Tests\ApiTester;
 use Codeception\Util\HttpCode;
 
@@ -19,7 +20,59 @@ class ApiSignupPostCest
         $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseContainsJson([
             'code' => HttpCode::OK,
-            'message' => 'Please check your email inbox and follow verification link',
+            'message' => 'Please check your email inbox and follow verification link.',
+        ]);
+    }
+
+    public function testInvalidEmail(ApiTester $I): void
+    {
+        $I->wantToTest('POST /api/signup (invalid email)');
+        $I->sendPOST('/api/signup', [
+            'email' => 'hello, world',
+            'password' => 'userStrongPassword',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
+        $I->seeResponseContainsJson([
+            'code' => HttpCode::BAD_REQUEST,
+            'message' => 'Please specify correct email address.',
+        ]);
+    }
+
+    public function testEmailAlreadySignedUp(ApiTester $I): void
+    {
+        $I->wantToTest('POST /api/signup (email already signed up)');
+
+        $user = $I->createUser();
+        $user->setEmail('signup@gmail.com');
+        $I->haveInRepository($user);
+
+        $I->sendPOST('/api/signup', [
+            'email' => 'signup@gmail.com',
+            'password' => 'userStrongPassword',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::CONFLICT);
+        $I->seeResponseContainsJson([
+            'code' => HttpCode::CONFLICT,
+            'message' => 'Such email already signed up.',
+        ]);
+    }
+
+    public function testVerificationEmailExist(ApiTester $I): void
+    {
+        $I->wantToTest('POST /api/signup (verification email already exist)');
+
+        $lead = new Lead();
+        $lead->setEmail('signup@gmail.com')->setVerifiedEmailAt(null);
+        $I->haveInRepository($lead);
+
+        $I->sendPOST('/api/signup', [
+            'email' => 'signup@gmail.com',
+            'password' => 'userStrongPassword',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+        $I->seeResponseContainsJson([
+            'code' => HttpCode::FORBIDDEN,
+            'message' => 'Please check your email inbox and follow verification link.',
         ]);
     }
 }
