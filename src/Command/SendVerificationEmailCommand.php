043f55a43a901f02d0b35687e97d1f89f8932f8e
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2021-04-07 19:44:32 +0300

    [SCD-109] update console command to send verification URL and set flag in DB
    Signed-off-by: Oleg Sklyarov <oleg.sklyarov@gmail.com>

--- src/Command/SendVerificationEmailCommand.php
+++ src/Command/SendVerificationEmailCommand.php
@@ -4,58 +4,55 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Service\Lead\LeadService;
 use App\Service\Signup\EmailNotificationInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class SendVerificationEmailCommand extends Command
 {
     protected static $defaultName = 'signup:send-verification-email';
 
+    // TODO replace with real verification url
     private const DEFAULT_VERIFICATION_LINK = 'https://ya.ru';
 
-    private const OPTION_EMAIL = 'email';
-    private const OPTION_PHONE = 'phone';
-
-    public function __construct(private EmailNotificationInterface $emailNotification)
-    {
+    public function __construct(
+        private EmailNotificationInterface $emailNotification,
+        private LeadService $leadService,
+        private EntityManagerInterface $entityManager,
+    ) {
         parent::__construct();
     }
 
     public function configure(): void
     {
-        $this
-            ->setDescription('Send email using SendPulse service to verify user email')
-            ->addOption(
-                self::OPTION_EMAIL,
-                null,
-                InputOption::VALUE_REQUIRED,
-                'email address to send verification link',
-                null
-            )
-            ->addOption(
-                self::OPTION_PHONE,
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'phone number of the user',
-            );
+        $this->setDescription('Send email using SendPulse service to verify user email');
     }
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $email = $input->getOption(self::OPTION_EMAIL);
-        if (null === $email) {
-            $output->writeln('please specify email parameter');
-
-            return 0;
+        $this->entityManager->getConnection()->beginTransaction();
+        try {
+            $leadsToNotify = $this->leadService->findLeadsForVerificationEmail();
+            foreach ($leadsToNotify as $lead) {
+                $output->writeln(sprintf('Sending verification URL to %s...', $lead->getEmail()));
+                $this->emailNotification->sendVerificationEmail($lead->getEmail(), null, self::DEFAULT_VERIFICATION_LINK);
+                $lead->setVerificationEmailSentAt(new \DateTimeImmutable());
+                $this->entityManager->persist($lead);
+            }
+            $this->entityManager->flush();
+            $this->entityManager->getConnection()->commit();
+        } catch (\Throwable $e) {
+            $this->entityManager->getConnection()->rollBack();
+            throw $e;
         }
 
-        $phone = $input->getOption(self::OPTION_PHONE);
-        $output->writeln(sprintf('Sending verification link to email %s...', $email));
-        $this->emailNotification->sendVerificationEmail($email, $phone, self::DEFAULT_VERIFICATION_LINK);
-        $output->writeln('DONE');
+        $output->writeln(empty($leadsToNotify)
+            ? 'No leads waiting for verification URL.'
+            : 'Done.'
+        );
 
         return 0;
     }
