b2f8c2a031d5204967622f660923cec0b9f14a26
ZhdanovO <oleg.zhdanov88@gmail.com>
2021-04-20 07:13:12 +0300

    [SCD-106] fix problem with SignupParamResolver.

--- src/Controller/Signup/SignupParamResolver.php
+++ src/Controller/Signup/SignupParamResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller\Signup;
 
-use App\Entity\Lead;
 use App\Service\Signup\SignupService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
@@ -13,6 +12,7 @@ use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 class SignupParamResolver implements ArgumentValueResolverInterface
 {
     private const PARAM_EMAIL = 'email';
+    private const PARAM_PASSWORD = 'password';
 
     public function __construct(private SignupService $signupService)
     {
@@ -20,12 +20,15 @@ class SignupParamResolver implements ArgumentValueResolverInterface
 
     public function supports(Request $request, ArgumentMetadata $argument): bool
     {
-        return Lead::class === $argument->getType() && null !== $request->get(self::PARAM_EMAIL);
+        return SignupDTO::class === $argument->getType() && null !== $request->get(self::PARAM_EMAIL) && null !== $request->get(self::PARAM_PASSWORD);
     }
 
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        $paramEmail = $request->get(self::PARAM_EMAIL);
-        yield $this->signupService->findByEmail($paramEmail);
+        $signupDTO = new SignupDTO();
+        $signupDTO->email = $request->get(self::PARAM_EMAIL);
+        $signupDTO->password = $request->get(self::PARAM_PASSWORD);
+
+        yield $signupDTO;
     }
 }
