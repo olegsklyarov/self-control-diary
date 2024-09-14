88da1b11b54f85df9848b78659c1ee95411ed188
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2024-09-14 07:39:21 +0300

    symfony bump to 5.4

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
